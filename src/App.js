a7f144ba65af0f2924137a2aff9efca6c51c3977
Gustavo Xavier <gustavohsx07@gmail.com>
2023-06-12 20:43:20 -0300

    Criando e estilizando Componentes Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./components/pages/Home"
 import Company from "./components/pages/Company"
 import Contact from "./components/pages/Contact"
 import NewProject from "./components/pages/NewProject"
 import Container from './components/layout/Container'
+import Projects from './components/pages/Projects'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router> 
-      <div>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/Company">Empresa</Link>
-          <Link to="/Contact">Contato</Link>
-          <Link to="/NewProject">Novo Projeto</Link>
-        </ul>
-      </div>
+      <Navbar />
         <Container customClass="min-heigth">
           <Routes>
             <Route exact path="/" element={<Home/>}/> 
             <Route path="/Company" element={<Company/>}/> 
             <Route path="/Contact" element={<Contact/>}/> 
-            <Route path="/NewProject" element={<NewProject/>}/>    
+            <Route path="/NewProject" element={<NewProject/>}/>
+            <Route path="/Projects" element={<Projects/>}/>    
           </Routes>
         </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
